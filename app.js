1073d8e050b190bd087b9d20b56468d2a92d1521
Bruno.S.Silva <brunosch250@gmail.com>
2023-01-17 20:21:18 -0300

    adding app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// // Este arquivo será usado para para o locahost com o Node JS / Nodemon (sem depender do Live Server).
+
+// var http = require('http');
+// // O código acima faz a requisição do módulo HTTP (nativo do NodeJs) e inicializa numa variável.
+
+// // Const é umavariável não alterável, e o m[odulo express (framework) ajuda no desenvolvimento web com o necessário, e claro, auxilia no back-end (futuramente será mostrado).
+// const express = require('express');
+// const app = express();
+
+
+
+
+// // Aqui, a variável com este módulo cria um servidor, usando uma função (req, res) que, vai mostrar a msg abaixo e abrir na porta desejada, ouseja, 8080.
+// http.createServer(function(req, res){
+//     res.end("Server aberto com sucesso");
+// }).listen();
+
+// //Rotas
+// app.get('/', function(req, res){
+//     res.render('pagamento');
+// });
+
+// app.listen(3010);
+
+
+// Código obsoleto, necvessário verificar com o Celke
